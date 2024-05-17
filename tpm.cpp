1b405d0886848cb22175d13d641d1ce7a8823b55
STR <nmzskacheats@gmail.com>
2024-05-16 20:29:49 -0700

    Update and rename main.cpp to tpm.cpp

--- tpm.cpp
+++ tpm.cpp
@@ -3,8 +3,40 @@
 
 #pragma comment(lib, "Tpm.lib")
 
+bool IsRunningAsAdmin()
+{
+    BOOL fIsElevated = FALSE;
+    HANDLE hToken = NULL;
+    TOKEN_ELEVATION elevation;
+    DWORD dwSize;
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+        std::cerr << "Failed to get Process Token: " << GetLastError() << std::endl;
+        return false;  // if fail then falsae
+    }
+
+    if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
+    {
+        std::cerr << "Failed to get Token Information: " << GetLastError() << std::endl;
+        CloseHandle(hToken);
+        return false;  //if fail then false
+    }
+
+    fIsElevated = elevation.TokenIsElevated;
+
+    CloseHandle(hToken);
+    return fIsElevated;
+}
+
 int main()
 {
+    if (!IsRunningAsAdmin())
+    {
+        std::cerr << "This program must be run as an administrator." << std::endl;
+        return 1;  // exit with error code
+    }
+
     // fetch tpm API
     TPMS_API_VERSION tpmApiVersion = { 2, 0 };
     HRESULT hr = TpmInitialize(&tpmApiVersion);
